cdce69a0ec5e2a960720c996c3e3e02171e825d3
Trey <treyvis@gmail.com>
2017-12-10 21:35:39 -0700

    Cleaning input for numeric only and adding commas

--- App.js
+++ App.js
@@ -6,23 +6,32 @@ import {
   TextInput
 } from 'react-native';
 
+import stripchar from 'stripchar';
+import commaNumber from 'comma-number';
+
 export default class App extends React.Component {
 
   state={
     purchasePrice: ''
   }
 
+  cleanNum = text => {
+    return commaNumber(stripchar.StripChar.RSExceptNum(text) || '');
+  }
+
   render() {
+    //this.cleanNum('1234f345');
     return (
       <View style={styles.container}>
         <Text style={styles.header}>Future home ROI</Text>
         <View style={styles.curencyContainer}>
           <Text style={styles.formInput}>$</Text>
           <TextInput 
+            keyboardType={'numeric'}
             style={styles.formInput}
             placeholder="Purchase price" 
             value={this.state.purchasePrice}
-            onChangeText={(text) => this.setState({purchasePrice: text})}/>
+            onChangeText={(text) => this.setState({purchasePrice: this.cleanNum(text)})}/>
         </View>
         <View style={styles.curencyContainer}>
           <Text style={styles.formInput}>$</Text>
